3843974605814abf9dbdc16ec413bb0a20a7af6e
guanxinshuai <guanxs@163.com>
2019-11-20 14:15:59 +0800

    init project

---
+++ boot-ssm-consumer/src/main/java/com/guan/BootSsmConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.guan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSsmConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSsmConsumerApplication.class, args);
+	}
+
+}
